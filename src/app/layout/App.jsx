ecbb68edb7667939403eeb10f158bb67323b2ba1
Abhinesh AV <email@abhinesh.com>
2018-07-07 22:12:44 +0530

    first push

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Re-vents 0 </h1>
+        hello
+      </div>
+    );
+  
+  }
+}
+
+export default App;
