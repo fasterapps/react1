73e2c735d0450468450a144f68af54d3807101ee
Abhinesh AV <email@abhinesh.com>
2018-07-16 12:54:17 +0530

    End of Section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import { Route, Switch } from "react-router-dom";
 
-import EventDashboard from '../../features/events/EventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar/NavBar'
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+
+import EventDetailedPage from "../../features/events/EventDetailed/EventDetailedPage";
+import EventForm from "../../features/events/EventForm/EventForm";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import UserDetailed from "../../features/user/UserDetailed/UserDetailed";
+import HomePage from "../../features/home/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <Container className="main">
-          <EventDashboard/>
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event:id" component={EventDetailedPage} />
+                  <Route path="/People" component={PeopleDashboard} />
+                  <Route path="/Profile/:id" component={UserDetailed} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
-  
   }
 }
 
---
+++ src/features/user/Settings/BasicPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BasicPage = () => {
+  return (
+    <div>
+       <h1>basic Page</h1>
+    </div>
+  )
+}
+
+export default BasicPage
