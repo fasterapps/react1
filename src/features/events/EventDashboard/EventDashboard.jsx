89084666104cf9d77011f2f0deb066d665066c67
Abhinesh AV <email@abhinesh.com>
2018-07-19 22:00:07 +0530

    Section 8 end

--- src/features/events/EventDashboard/EventDashboard.jsx
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -1,100 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Grid, Button } from "semantic-ui-react";
-import cuid from "cuid";
+import { Grid } from "semantic-ui-react";
+
 import EventList from "../EventList/EventList";
-import EventForm from "../EventForm/EventForm";
-import { createEvent, updateEvent, deleteEvent } from "../eventActions";
+
+import { deleteEvent } from "../eventActions";
 
 const mapState = state => ({
   events: state.events
 });
 
 const actions = {
-  createEvent,
-  updateEvent,
   deleteEvent
 };
 
 class EventDashboard extends Component {
-  state = {
-    isOpen: false,
-    selectedEvent: null
-  };
-
-  handleFormOpen = () => {
-    this.setState({
-      selectedEvent: null,
-      isOpen: true
-    });
-  };
-
-  handleFormCancel = () => {
-    this.setState({
-      isOpen: false
-    });
-  };
-
-  handleUpdateEvent = updatedEvent => {
-    this.props.updateEvent(updatedEvent);
-    this.setState({
-      selectedEvent: null,
-      isOpen: false
-    });
-  };
-
-  handleOpenEvent = eventToOpen => () => {
-    this.setState({
-      selectedEvent: eventToOpen,
-      isOpen: true
-    });
-  };
-
-  handleCreateEvent = newEvent => {
-    newEvent.id = cuid();
-    newEvent.hostPhotoURL = "/assets/user.png";
-    this.props.createEvent(newEvent);
-    this.setState({
-            isOpen: false
-    });
-  };
 
   handleDeleteEvent = eventId => () => {
-   this.props.deleteEvent(eventId);
+    this.props.deleteEvent(eventId);
   };
 
   render() {
-    const { selectedEvent } = this.state;
     const { events } = this.props;
 
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList
-            deleteEvents={this.handleDeleteEvent}
-            onEventOpen={this.handleOpenEvent}
-            events={events}
-          />
+          <EventList deleteEvents={this.handleDeleteEvent} events={events} />
         </Grid.Column>
 
-        <Grid.Column width={6}>
-          <Button
-            onClick={this.handleFormOpen}
-            positive
-            content="Create Event"
-          />
-          {this.state.isOpen && (
-            <EventForm
-              updateEvent={this.handleUpdateEvent}
-              selectedEvent={selectedEvent}
-              createEvent={this.handleCreateEvent}
-              handleCancel={this.handleFormCancel}
-            />
-          )}
-        </Grid.Column>
+        <Grid.Column width={6} />
       </Grid>
     );
   }
 }
 
-export default connect(mapState, actions)(EventDashboard);
+export default connect(
+  mapState,
+  actions
+)(EventDashboard);
